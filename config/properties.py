9418ae15c985927fff609d0220f716d97318c489
Daniel Suveges <suvi.dani@gmail.com>
2019-07-08 10:58:20 +0100

    Updating config file.

--- config/properties.py
+++ config/properties.py
@@ -7,17 +7,6 @@ class Configuration():
     rootPath = os.path.dirname(os.path.realpath(__file__))
     uploadFolder = '{}/../uploadedTemplates'.format(rootPath)  #'/Users/dsuveges/Project/gwas-template-services/uploadedTemplates'
 
-    # Credentials for the private sftp:
-    FTP = {
-        'ftpServer': 'ftp-private.ebi.ac.uk',
-        'ftpUser': 'gwas_cat',
-        'ftpPassword': 'y9vYPfvg'
-    }
-
-    # GWAS deposition service app:
-    serviceAppAddress = 'http://snoopy.ebi.ac.uk'
-    serviceAppPort = '5000'
-
     # List of schemas that needs to be served:
     schemas = {
         'study': '/schema_definitions/study_schema.xlsx',
@@ -27,13 +16,6 @@ class Configuration():
     }
 
     # Template filter definitions:
-    # {
-    #     "curator": true,
-    #     "haplotype": true,
-    #     "snpxsnp": true,
-    #     "effect": "beta", // or "OR"
-    #     "backgroundTrait": true,
-    # }
     filters = {
         'curator' : {
             'note' : ['study_tag', 'note', 'note_subject', 'status'],
