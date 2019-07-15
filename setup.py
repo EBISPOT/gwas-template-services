fe226f0a4d433c22774016923a8be89c6b85d90e
Daniel Suveges <suvi.dani@gmail.com>
2019-07-15 11:58:06 +0100

    Schema datafiles are added to the package. A separate package is created to manage schema versions: get a list of schema versions, open schema files for a given version.

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ included in this repository
 setup(
     name='templateServices',
     version='0.1',
-    packages=['config', 'validation', 'template','schema_definitions'],
+    include_package_data=True,
+    package_data={ 'schema_definitions' : ['*.xlsx'] },
+    packages=['config', 'template','schema_definitions'],
     entry_points={
         "console_scripts": ['gwas-spreadsheet_builder = template.spreadsheet_builder:main',
                             'gwas-JSON_schema_builder = template.schemaJson_builder:main',
-                            'gwas-validator = validation.validator:main'
                             'gwas_schema_definitions = schema_definitions.schemaVersion:main']
     },
     url='https://github.com/EBISPOT/gwas-template-services',
@@ -20,5 +21,5 @@ setup(
     author='Daniel Suveges',
     author_email='dsuveges@ebi.ac.uk',
     description='Package to manage template related services for the data deposition system of the GWAS Catalog',
-    install_requires=['pandas==0.22.0', 'xlsxwriter==1.1.8', 'flask', 'gunicorn', 'eventlet', 'pytest-cov']
+    install_requires=['pandas==0.22.0', 'xlsxwriter==1.1.8', 'flask', 'gunicorn', 'eventlet', 'pytest-cov'],
 )
