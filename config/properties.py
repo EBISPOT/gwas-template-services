26fb5ddbda76c8e53a8081f2700582847ccd9cf0
ala <63405719+ala-ebi@users.noreply.github.com>
2022-03-02 15:03:11 +0000

    indentation

--- config/properties.py
+++ config/properties.py
@@ -44,8 +44,7 @@ class Configuration():
         },
         'curatorSummaryStats': {
             'removeColumn': {
-                'study': ['background_trait', 'efo_trait', 'array_manufacturer', 'genotyping_technology', 'efo_trait',
-                          'background_efo_trait',
+                'study': ['background_trait', 'efo_trait', 'array_manufacturer', 'genotyping_technology', 'efo_trait', 'background_efo_trait',
                           'array_information', 'imputation', 'variant_count', 'statistical_model', 'study_description', 'gxe_flag', 'pooled_flag'],
                 'sample': ['study_tag', 'stage', 'size', 'cases', 'controls', 'sample_description', 'ancestry_category',
                            'ancestry', 'ancestry_description', 'country_recruitement'],
