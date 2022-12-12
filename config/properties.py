5624311ec171d7fa182fda88c8ae63ea0726f6d3
jdhayhurst <hayhurst.jd@gmail.com>
2022-12-12 16:33:12 +0000

    add genotyping tech to sumstats study sheet

--- config/properties.py
+++ config/properties.py
@@ -29,7 +29,7 @@ class Configuration():
         },
         'summaryStats' : {
             'removeColumn': {
-                'study': [ 'background_trait', 'efo_trait','array_manufacturer', 'genotyping_technology','efo_trait', 'background_efo_trait',
+                'study': [ 'background_trait', 'efo_trait','array_manufacturer','efo_trait', 'background_efo_trait',
                           'array_information', 'imputation', 'variant_count', 'statistical_model', 'study_description', 'gxe_flag', 'pooled_flag'],
                 'sample': ['study_tag', 'stage', 'size', 'cases', 'controls', 'case_control_study', 'sample_description', 'ancestry_category',
                            'ancestry', 'ancestry_description', 'ancestry_method', 'country_recruitement'],
@@ -44,7 +44,7 @@ class Configuration():
         },
         'curatorSummaryStats': {
             'removeColumn': {
-                'study': ['background_trait', 'efo_trait', 'array_manufacturer', 'genotyping_technology', 'efo_trait', 'background_efo_trait',
+                'study': ['background_trait', 'efo_trait', 'array_manufacturer', 'efo_trait', 'background_efo_trait',
                           'array_information', 'imputation', 'variant_count', 'statistical_model', 'study_description', 'gxe_flag', 'pooled_flag'],
                 'sample': ['study_tag', 'stage', 'size', 'cases', 'controls', 'case_control_study','sample_description', 'ancestry_category',
                            'ancestry', 'ancestry_description', 'ancestry_method', 'country_recruitement'],
