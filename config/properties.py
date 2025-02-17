5c9dd445f0567563a1746424addde326e9f316e9
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2025-02-17 17:30:13 +0000

    fix: Make coordinate system mandatory

--- config/properties.py
+++ config/properties.py
@@ -76,7 +76,7 @@ class Configuration():
     }
 
     # Schema version:
-    schemaVersion = "1.11"
+    schemaVersion = "1.12"
 
     # Data line separator:
     triggerRow = "Add your data below this line"
