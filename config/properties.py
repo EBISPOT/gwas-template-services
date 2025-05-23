a2eb6c83f2a5fa93a75f9baf540df8c64e5ffab7
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2025-05-23 15:23:50 +0100

    fix: Allow NR for coordinate system

--- config/properties.py
+++ config/properties.py
@@ -76,7 +76,7 @@ class Configuration():
     }
 
     # Schema version:
-    schemaVersion = "1.14"
+    schemaVersion = "1.15"
 
     # Data line separator:
     triggerRow = "Add your data below this line"
